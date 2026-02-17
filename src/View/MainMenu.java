81a62d71818c651eb4a8e81f2142fd2979f16089
FireBlood <tristan.turki4@gmail.com>
2026-02-17 10:03:17 +0100

    ajout fichier MainMenuController

--- src/View/MainMenu.java
+++ src/View/MainMenu.java
@@ -9,8 +9,7 @@ import java.awt.*;
 public class MainMenu {
     private JFrame frame = new JFrame("Java MiniGames");
     private JPanel mainPanel = new GradientPanel();
-    private JPanel topPanel;
-    private JPanel centerPanel;
+    private JPanel centerPanel = new GradientPanel();
     private JButton navalButton;
     private JButton shifumiButton;
     private JButton guessButton;
@@ -18,6 +17,7 @@ public class MainMenu {
     private JLabel menuLabel;
 
     public MainMenu() {
+        mainPanel.setLayout(new BorderLayout());
         frame.setContentPane(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
@@ -25,11 +25,26 @@ public class MainMenu {
         frame.setVisible(true);
         frame.setMinimumSize(new Dimension(800, 600));
 
+        centerPanel.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+
+        menuLabel = new JLabel("Select a game");
+        navalButton = new JButton("Naval Battle");
+        shifumiButton = new JButton("ShiFuMi");
+        guessButton = new JButton("Guess the number");
+        calculatorButton = new JButton("Calculator");
+
+        mainPanel.add(menuLabel, BorderLayout.NORTH);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+        centerPanel.add(navalButton);
+
         StyleModel.addMainMenuStyleToLabel(menuLabel);
         StyleModel.addMainMenuStyleToButton(navalButton);
         StyleModel.addMainMenuStyleToButton(shifumiButton);
         StyleModel.addMainMenuStyleToButton(guessButton);
         StyleModel.addMainMenuStyleToButton(calculatorButton);
+
+
     }
 
 }
